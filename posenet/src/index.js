a206b1707c3963dafa24a0b3ea1239bdb53ea240
zac <zachary.gagnou@epfedu.fr>
2022-02-19 15:45:26 +0100

    correct usage of p5 in RactDOM

--- posenet/src/index.js
+++ posenet/src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ml5 from "ml5";
+import p5 from 'p5';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const s = ( sketch ) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  let video;
+  let poseNet;
+  let pose;
+  let skeleton;
+
+  sketch.setup = () => {
+    sketch.createCanvas(640, 480);
+    video = sketch.createCapture(p5.video)
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  };
+
+  function modelLoaded() {
+    console.log('poseNet ready')
+  }
+
+  function gotPoses(poses){
+    if (poses.length > 0) {
+      pose = poses[0].pose;
+      skeleton = poses[0].skeleton;
+    }
+  }
+
+  sketch.draw = () => {
+    sketch.image(video, 0, 0);
+
+    if (pose) {
+      let eyeR = pose.rightEye;
+      let eyeL = pose.leftEye;
+      let d = sketch.dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+      sketch.fill(255, 0, 0);
+      sketch.ellipse(pose.nose.x, pose.nose.y, d);
+      sketch.fill(0, 0, 255);
+      sketch.ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
+      sketch.ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
+      
+      for (let i = 0; i < pose.keypoints.length; i++) {
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        sketch.fill(0,255,0);
+        sketch.ellipse(x,y,16,16);
+      }
+      
+      for (let i = 0; i < skeleton.length; i++) { 
+        let a = skeleton[i][0];
+        let b = skeleton[i][1];
+        sketch.strokeWeight(2);
+        sketch.stroke(255);
+        sketch.line(a.position.x, a.position.y,b.position.x,b.position.y);     
+      }
+    }
+
+  };
+};
+
+let myp5 = new p5(s);
+
+ReactDOM.render(myp5, document.getElementById('root'));
